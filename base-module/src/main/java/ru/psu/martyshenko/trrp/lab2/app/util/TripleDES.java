c53936a90627cc74263a67b768d5afe6e1e3b8a2
limonpudding <limonpudding@gmail.com>
2020-11-06 13:31:26 +0500

    Доработки конфигурации.

--- base-module/src/main/java/ru/psu/martyshenko/trrp/lab2/app/util/TripleDES.java
+++ base-module/src/main/java/ru/psu/martyshenko/trrp/lab2/app/util/TripleDES.java
@@ -1,11 +1,11 @@
 package ru.psu.martyshenko.trrp.lab2.app.util;
-import java.security.MessageDigest;
-import java.util.Arrays;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.MessageDigest;
+import java.util.Arrays;
 
 public class TripleDES {
 
--- src/main/java/ru/psu/martyshenko/trrp/lab2/app/Application.java
+++
@@ -1,7 +0,0 @@
-package ru.psu.martyshenko.trrp.lab2.app;
-
-public class Application {
-    public static void main(String[] args) {
-        System.out.println("nothing");
-    }
-}
